0a4c69f257a09b50cf79e4a9a2632cbf866fd909
Sidao <svisentini@gmail.com>
2021-03-26 00:22:00 -0300

    Revisao Inicial

---
+++ src/main/java/com/algaworks/algafoodtestapi/AlgafoodTestApiApplication.java
@@ -0,0 +1,13 @@
+package com.algaworks.algafoodtestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgafoodTestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgafoodTestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/algaworks/algafoodtestapi/AlgafoodTestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.algaworks.algafoodtestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgafoodTestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
